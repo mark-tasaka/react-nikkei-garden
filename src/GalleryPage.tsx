cbf79803f78f58d45ae18a17adfcf63a601f7f4b
Mark Tasaka <mark.tasaka@gmail.com>
2026-07-15 13:34:21 -0700

    new photos added

--- src/GalleryPage.tsx
+++ src/GalleryPage.tsx
@@ -393,6 +393,10 @@ import donors_2  from './img/gallery/donors/donors_2.jpeg';
 import donors_3  from './img/gallery/donors/donors_3.jpeg';
 import donors_4  from './img/gallery/donors/donors_4.jpeg';
 import donors_5  from './img/gallery/donors/donors_5.jpeg';
+import donors_6  from './img/gallery/donors/donors_6.jpeg';
+import donors_7  from './img/gallery/donors/donors_7.jpeg';
+import donors_8  from './img/gallery/donors/donors_8.jpeg';
+import donors_9  from './img/gallery/donors/donors_9.jpeg';
 
 interface GalleryImage {
   src: string;
@@ -516,6 +520,10 @@ const DONORS_IMAGES: GalleryImage[] = [
   { src: donors_3, alt: 'Donors' },
   { src: donors_4, alt: 'Donors' },
   { src: donors_5, alt: 'Donors' },
+  { src: donors_6, alt: 'Donors' },
+  { src: donors_7, alt: 'Donors' },
+  { src: donors_8, alt: 'Donors' },
+  { src: donors_9, alt: 'Donors' },
 ];
 
 const OPENING_IMAGES: GalleryImage[] = [
